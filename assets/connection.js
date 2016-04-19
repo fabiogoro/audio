4707e12b499ba9c8107e3fb295a5f6fff2b269da
fabio gorodscy <fabiog@ime.usp.br>
2016-04-19 16:47:49 -0300

    Show position when moving mouse

--- assets/connection.js
+++ assets/connection.js
@@ -5,6 +5,7 @@ var ws       = new WebSocket(uri);
 ws.onmessage = function(message) {
   var data = JSON.parse(message.data);
   frequencies[data.frequency].gain.gain.value = data.value;
+  frequencies[data.frequency].element.css('padding', data.value*100+'%');
 };
 
 function send(data){
