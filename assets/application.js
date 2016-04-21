803d9108088d695105f45a828c8461bbc6cc05c7
fabio gorodscy <fabiog@ime.usp.br>
2016-04-21 14:45:58 -0300

    Moving x and y

--- assets/application.js
+++ assets/application.js
@@ -3,16 +3,9 @@ var my_frequency;
 
 my_frequency = get_random_frequency();
 
-$(document).mousemove(function(document) {
-  mouse_pos_value = get_mouse_pos_value(document);
-  send({ value: mouse_pos_value, frequency: my_frequency});
+$(document).mousemove(function(mouse) {
+  send({ x: mouse.pageX/$(window).width(), y: mouse.pageY/$(window).height(), frequency: my_frequency});
 });
-
-function get_mouse_pos_value(element){
-  width_value = element.pageX/$(window).width();
-  height_value = element.pageY/$(window).height();
-  return (width_value+height_value)/2;
-}
 
 function get_random_frequency(){
   return Math.floor(Math.random() * frequencies.length);
--- assets/connection.js
+++ assets/connection.js
@@ -4,8 +4,8 @@ var ws       = new WebSocket(uri);
 
 ws.onmessage = function(message) {
   var data = JSON.parse(message.data);
-  frequencies[data.frequency].gain.gain.value = data.value;
-  frequencies[data.frequency].element.css('padding', data.value*100+'%');
+  frequencies[data.frequency].gain.gain.value = (data.x + data.y)/2;
+  frequencies[data.frequency].element.css('padding', data.y*100+'% '+data.x*100+'%');
 };
 
 function send(data){
