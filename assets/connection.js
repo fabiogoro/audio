e6c3981c624284d964ad429cf13e602c796692d2
fabio gorodscy <fabiog@ime.usp.br>
2016-04-24 10:23:10 -0300

    updating git

--- assets/connection.js
+++ assets/connection.js
@@ -4,10 +4,19 @@ var ws       = new WebSocket(uri);
 
 ws.onmessage = function(message) {
   var data = JSON.parse(message.data);
-  frequencies[data.frequency].gain.gain.value = (data.x + data.y)/2;
-  frequencies[data.frequency].element.css('padding', data.y*100+'% '+data.x*100+'%');
+  $('#messages').prepend(data.som+'</br>');
+  var buffer = data.som.toLowerCase().replace(/[^a-z]+/g, '');
+  play(buffer);
+  //window.setTimeout(play,data.time-initial_time-performance.now()+1000);
 };
 
 function send(data){
   ws.send(JSON.stringify(data));
 }
+
+var initial_time;
+
+$(function (){
+  var now = new Date().getTime();
+  initial_time = now - performance.now();
+});
--- assets/webaudio.js
+++ assets/webaudio.js
@@ -1,39 +1,77 @@
+//querty 
+//trabalho desenvolvido a partir de samples retirados da leitura de Hroldo de campos do poema galáxias, 
+// Dezembro de 2014
+//por Ariane Stolfi
+
+//não é compatível com todos browsers
+
+//fique a vontade pra copiar o codigo
+//direitos autorais dos samples e video pertencem aos seus respectivos autores
+
+//work in progress
+var sample = {};
 var audio_context = new (window.AudioContext || window.webkitAudioContext)();
 
-var frequencies = [{ frequency: 50, gain: audio_context.createGain(), element: $('#a0') },
-             { frequency: 150, gain: audio_context.createGain(), element: $('#a1')  },
-             { frequency: 250, gain: audio_context.createGain(), element: $('#a2')  },
-             { frequency: 350, gain: audio_context.createGain(), element: $('#a3')  },
-             { frequency: 450, gain: audio_context.createGain(), element: $('#a4')  },
-             { frequency: 550, gain: audio_context.createGain(), element: $('#a5')  },
-             { frequency: 650, gain: audio_context.createGain(), element: $('#a6')  },
-             { frequency: 750, gain: audio_context.createGain(), element: $('#a7')  },
-             { frequency: 850, gain: audio_context.createGain(), element: $('#a8')  },
-             { frequency: 1050, gain: audio_context.createGain(), element: $('#a9')  },
-             { frequency: 1150, gain: audio_context.createGain(), element: $('#a10')  },
-             { frequency: 1250, gain: audio_context.createGain(), element: $('#a11')  },
-             { frequency: 1350, gain: audio_context.createGain(), element: $('#a12')  },
-             { frequency: 1450, gain: audio_context.createGain(), element: $('#a13')  },
-             { frequency: 1550, gain: audio_context.createGain(), element: $('#a14')  },
-             { frequency: 1650, gain: audio_context.createGain(), element: $('#a15')  },
-             { frequency: 1750, gain: audio_context.createGain(), element: $('#a16')  },
-             { frequency: 1850, gain: audio_context.createGain(), element: $('#a17')  },
-             { frequency: 1950, gain: audio_context.createGain(), element: $('#a18')  },
-             { frequency: 2050, gain: audio_context.createGain(), element: $('#a19')  }];
+function chat(event) {
+  if(event.keyCode===13){
+    var text = $('#chat').val();
+    $('#chat').val('');
+    send({som: text, time: new Date().getTime()});
+  }
+};
 
 $(function(){
-  function setup(frequency) {
-    var oscillator = audio_context.createOscillator();
-    oscillator.connect(frequency.gain);
-    frequency.gain.gain.value = 0;
-
-    oscillator.type = 'sine';
-    oscillator.frequency.value = frequency.frequency;
-    oscillator.start(0);
-
-    frequency.gain.connect(audio_context.destination);
-  }
-  for(var i=0;i<frequencies.length;i++){
-    setup(frequencies[i]);
-  }
+  load('a');
+  load('b');
+  load('c');
+  load('d');
+  load('e');
+  load('f');
+  load('g');
+  load('h');
+  load('i');
+  load('j');
+  load('k');
+  load('l');
+  load('m');
+  load('n');
+  load('o');
+  load('p');
+  load('q');
+  load('r');
+  load('s');
+  load('t');
+  load('u');
+  load('v');
+  load('w');
+  load('x');
+  load('y');
+  load('z');
 });
+  
+function play(buffer){
+  if(buffer){
+    var source = audio_context.createBufferSource();
+    source.buffer = sample[buffer[0]];
+    source.connect(audio_context.destination);
+    source.onended = function(){buffer = buffer.slice(1,buffer.length);play(buffer);};
+    source.start();
+  }
+}
+
+function load(file){
+  var request = new XMLHttpRequest();
+  request.onload = function() {
+    audio_context.decodeAudioData(request.response, function(buffer) {
+      sample[file] = buffer;
+    }, onError);
+  }
+  request.open('GET', 'samples/'+file+'.mp3', true);
+  request.responseType = 'arraybuffer';
+  request.send();
+}
+
+function onError(e){
+  console.log('Error: '+e);
+}
+
