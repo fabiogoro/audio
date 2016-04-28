cb39e347c5f1d07e1bc857d778a5e54ea745bbd0
fabio gorodscy <fabiog@ime.usp.br>
2016-04-27 21:10:19 -0300

    Fixed play on click and added stop function

--- assets/connection.js
+++ assets/connection.js
@@ -2,13 +2,20 @@ var scheme   = "wss://";
 var uri      = scheme + "websocketfaye.herokuapp.com/";
 var ws       = new WebSocket(uri);
 
+var color = 0;
 ws.onmessage = function(message) {
   var data = JSON.parse(message.data);
-  $('#messages').prepend('<p class="msg">'+data.text+'</p>');
-  var buffer = data.text.toLowerCase().replace(/[^a-z]+/g, '');
-  play(buffer);
+  if(data.show) {
+    $('#messages').prepend('<p class="msg color'+color+'">'+data.text+'</p>');
+    color = (color+1)%4;
+  }
+  if(data.text === 'stop!') buffer = []; else {
+    var text = data.text.toLowerCase().replace(/[^a-z.]+/g, '').split('');
+    buffer.push(text);
+    play(buffer.length-1);
+  }
 };
 
-function send(data){
-  ws.send(JSON.stringify(data));
+function send(data, show=0){
+  ws.send(JSON.stringify({text: data, show: show}));
 }
--- assets/webaudio.js
+++ assets/webaudio.js
@@ -1,44 +1,26 @@
 var sample = {};
-var audio_context = new (window.AudioContext || window.webkitAudioContext)();
+var buffer = [];
 
 function chat(event) {
   if(event.keyCode===13){
     var text = $('#chat').val();
     $('#chat').val('');
-    send({text: text});
+    if(text != '') send(text,1);
   }
 };
 
-$(function(){
-  var files = 'abcdefghijklmnopqrstuvwxyz';
-  for(var i=0; i<files.length; i++) load(files[i]);
+$(document).on('click', '.msg', function(){
+  $(this).toggleClass("on");
+  var text = $(this).text();
+  send(text);
 });
-  
-function play(buffer){
-  if(buffer){
-    var source = audio_context.createBufferSource();
-    source.buffer = sample[buffer[0]];
-    source.connect(audio_context.destination);
-    source.onended = function(){buffer = buffer.slice(1,buffer.length);play(buffer);};
-    source.start();
-  }
+
+function play(pos){
+  var source = audio_context.createBufferSource();
+  var sound = buffer[pos].shift();
+  if(sound === ' ') sound = 'space';
+  source.buffer = sample[sound];
+  source.connect(audio_context.destination);
+  source.onended = function(){if(buffer.length != 0 && buffer[pos] != '' ) play(pos);};
+  source.start(0);
 }
-
-function load(file){
-  var request = new XMLHttpRequest();
-  request.onload = function() {
-    audio_context.decodeAudioData(request.response, function(buffer) {
-      sample[file] = buffer;
-    }, onError);
-  }
-  request.open('GET', 'samples/'+file+'.mp3', true);
-  request.responseType = 'arraybuffer';
-  request.send();
-}
-
-function onError(e){
-  console.log('Error: '+e);
-}
-
-
-
