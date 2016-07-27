a17e8915989ed19f8d5bef9dbc34278c1ddb9cd2
fabio gorodscy <fabiog@ime.usp.br>
2016-07-26 23:14:51 -0300

    creating branch

--- assets/connection.js
+++ assets/connection.js
@@ -1,5 +1,19 @@
-var uri      = SERVER;
-var ws       = new WebSocket(uri);
+var pubnub = PUBNUB({
+    subscribe_key: 'sub-c-6f08dafe-ef69-11e5-872f-02ee2ddab7fe',
+    publish_key: 'pub-c-d499c43e-d44f-4daf-b7af-23414ef0019a'
+});
+
+pubnub.subscribe({
+  channel : "banda",
+  message : {text: '/samples', touch: 0},
+  callback: function(message) {
+    var data = message;
+    system_commands(data);
+  },
+  error: function(err) {
+    console.log(err);
+  }
+});
 
 var id = 0;
 var folder = 0;
@@ -8,35 +22,35 @@ var load_flag = 1;
 
 function send(data, touch){
   if(!touch) touch=0;
-  ws.send(JSON.stringify({text: data, touch: touch}));
+  pubnub.publish({
+    channel: 'banda',
+    message: {text: data, touch: touch},
+  });
 }
-
-ws.onmessage = function(message) {
-  var data = JSON.parse(message.data);
-  system_commands(data);
-};
-
-ws.onopen = function(message) {
-  send('/samples');
-};
 
 function loaded(){
   if(load_flag){
-    ws.onmessage = function(message) {
-      var data = JSON.parse(message.data);
-      if(!system_commands(data)) {
-        if(!data.touch) {
-          $('#messages').prepend('<p class="msg color'+id%4+'">'+data.text+'</p>');
-          id += 1;
+    pubnub.subscribe({
+      channel : "banda",
+      message : {text: '/samples', touch: 0},
+      callback: function(message) {
+        var data = message;
+        if(!system_commands(data)) {
+          if(!data.touch) {
+            $('#messages').prepend('<p class="msg color'+id%4+'">'+data.text+'</p>');
+            id += 1;
+          }
+          if(!simple_commands(data.text.toLowerCase())) {
+            var text = data.text.split('');
+            buffer.push(text);
+            play(buffer.length-1);
+          }
         }
-        if(!simple_commands(data.text.toLowerCase())) {
-          var text = data.text.split('');
-          buffer.push(text);
-          play(buffer.length-1);
-        }
-      }
-      if(!record) $('#messages p:nth-child(100)').remove();
-    };
+        if(!record) $('#messages p:nth-child(100)').remove();
+        var data = message;
+        system_commands(data);
+      },
+    });
 
     $('#main').show(500);
     $('#loading').hide(500);
